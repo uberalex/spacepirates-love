6c449e6253ec0396239289fae7cf4a2f81a8b61e
Alex O'Connor <oconnoat@gmail.com>
2012-12-04 18:45:14 +0000

    added planetary gravity

--- main.lua
+++ main.lua
@@ -24,12 +24,28 @@ function love.load()
     freq = 1
     objects.bullets = {}
 
+    -- planets
+    objects.planets = {}
+    
+    planet1 = {}
+    planet1.body = love.physics.newBody(world,100,100, "static")
+    planet1.shape = love.physics.newCircleShape(50)
+    planet1.fixture = love.physics.newFixture(planet1.body, planet1.shape, 1)
+
+    planet2 = {}
+    planet2.body = love.physics.newBody(world, 400,200, "static")
+    planet2.shape = love.physics.newCircleShape(50)
+    planet2.fixture = love.physics.newFixture(planet2.body, planet2.shape, 1)
+
+    table.insert(objects.planets, planet1)
+    table.insert(objects.planets, planet2)
+    
     -- screen setup
     love.graphics.setBackgroundColor(8, 8, 16 ) -- black space
     love.graphics.setMode(650, 650, false, true, 0)
 end
 
-function addBullet()
+function addBullet() -- shoot a bullet
     newBullet = {}
     newBullet.body = love.physics.newBody(world, objects.ship.body:getX(), objects.ship.body:getY() - 5, "dynamic")
     newBullet.shape = love.physics.newCircleShape(2) -- radius = 2
@@ -40,6 +56,16 @@ function addBullet()
             math.cos(objects.ship.body:getAngle()) * -250
     )
     table.insert(objects.bullets, newBullet)
+end
+
+function doGravity(planet, object) -- calculate the radial gravity for the planet
+    force = 10 -- the force of gravity
+    -- distance, x1, y1, x2, y2 = love.physics.getDistance(planet.fixture, object.fixture) -- THIS CRASHES FOR SOME REASON
+     local distance = math.sqrt( ( planet.body:getX() - object.body:getX() )^2 + (planet.body:getY() - object.body:getY()) ^2 ) -- calculate the mutual distance
+     local angle = math.atan2(planet.body:getX() - object.body:getX(), planet.body:getY() - object.body:getY()) -- calculate the mutual angle
+     local newForce = force / (distance^2)
+     
+     object.body:applyLinearImpulse( newForce * math.sin(angle), newForce * math.cos(angle) )
 end
 
 function love.update(dt)
@@ -74,6 +100,15 @@ function love.update(dt)
             table.remove(objects.bullets, i)
         end
     end
+    
+    -- planetary physics
+    for p, planet in ipairs(objects,planets) do
+        doGravity(planet, objects.ship)
+        for i,bullet in ipairs(object.bullets) do
+            doGravity(planet, bullet)
+        end
+    end
+
     -- keyboard
     if love.keyboard.isDown("right") then -- right arrow
         objects.ship.body:setAngle(
@@ -122,8 +157,14 @@ function love.draw()
     love.graphics.polygon("fill", objects.ship.body:getWorldPoints(objects.ship.shape:getPoints()))
 
     love.graphics.setColor(128, 128, 255) -- blue bullets
+
     for i,bullet in ipairs(objects.bullets) do
         love.graphics.circle("fill", bullet.body:getX(), bullet.body:getY(), bullet.shape:getRadius())
     end
+
+    love.graphics.setColor(128, 255, 128) 
     
+    for i,planet in ipairs(objects.planets) do
+        love.graphics.circle("fill", planet.body:getX(), planet.body:getY(), planet.shape:getRadius())
+    end
 end
